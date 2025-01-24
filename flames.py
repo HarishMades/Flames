9fec0f6daae0a96164832547016457293953b62d
HarishMades <harishmd448@gmail.com>
2025-01-24 12:19:58 +0530

    Flames game

---
+++ flames.py
@@ -0,0 +1,28 @@
+def names(Person_1, Person_2):
+    
+    first = list(Person_1)
+    
+    second = list(Person_2)
+
+    for i in Person_1:
+        
+        if i in second:
+                
+            first.remove(i)
+                
+            second.remove(i)      
+        
+
+    String = ["Friends", "Lovers", "Affection", "Marriage", "Enemies", "Siblings"]
+
+    remain = len(first) + len(second)
+
+    while len(String) > 1:
+        
+        index = (remain % len(String)) - 1
+        
+        String.remove(String[index])
+        
+        String = String[index + 1:] + String[:index]
+
+    print(String[0])
